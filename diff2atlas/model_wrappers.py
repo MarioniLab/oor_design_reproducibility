cd8b90d9b14f40a169e16f016b6d367290892dcb
Emma Dann <ed6@sanger.ac.uk>
2022-06-27 14:32:06 +0100

    making models and uncertainty apis

---
+++ diff2atlas/model_wrappers.py
@@ -0,0 +1,144 @@
+import numpy as np
+import pandas as pd
+from anndata import AnnData
+import scanpy as sc
+import torch
+import scvi
+import celltypist
+
+from pynndescent import NNDescent
+import pynndescent
+import pickle as pkl
+
+## --- scVI --- ##
+
+
+def train_scVI(
+        train_adata: AnnData,
+        outfile: str = None,
+        **kwargs):
+    '''
+    Train scVI model
+    Params:
+    ------
+    - train_adata: AnnData object of training data (already subset to highly variable genes)
+        counts should be stored in train_adata.layers['counts']
+    - outfile: path to dir to save trained model
+    - **kwargs: arguments for scvi.model.SCVI.setup_anndata (specifying batch etc)
+    '''
+
+    scvi.model.SCVI.setup_anndata(train_adata, layer='counts', **kwargs)
+
+    arches_params = dict(
+        use_layer_norm="both",
+        use_batch_norm="none",
+        encode_covariates=True,
+        dropout_rate=0.2,
+        n_layers=2,
+    )
+
+    model_train = scvi.model.SCVI(
+        train_adata,
+        **arches_params
+    )
+
+    model_train.train()
+    if outfile is not None:
+        model_train.save(outfile, overwrite=True)
+    return(model_train)
+
+
+def fit_scVI(
+    model_train: scvi.model.SCVI,
+    query_adata: AnnData,
+    outfile: str
+) -> scvi.model.SCVI:
+    '''
+    Fit query data to scVI model with scArches 
+    '''
+    vae_q = scvi.model.SCVI.load_query_data(
+        query_adata,
+        model_train,
+        inplace_subset_query_vars=True
+    )
+
+    vae_q.train(max_epochs=200, plan_kwargs=dict(weight_decay=0.0))
+    return(vae_q)
+
+
+## --- CellTypist --- ##
+
+def train_celltypist(
+        train_adata: AnnData,
+        labels_obs: str,
+        outfile: str = None,
+        **kwargs):
+    '''
+    Train celltypist model
+    Params:
+    ------
+    - train_adata: AnnData object of training data (already subset to highly variable genes)
+        counts should be stored in train_adata.layers['counts']
+    - labels_obs: column in obs storing cell type labels for classification
+    - outfile: path to pkl file to save trained model
+    - **kwargs: arguments for celltypist.train
+    '''
+
+    # Preprocessing
+    train_adata.X = train_adata.layers['counts'].copy()
+    sc.pp.normalize_total(train_adata, target_sum=10000)
+    sc.pp.log1p(train_adata)
+
+    # Train
+    ct_model = celltypist.train(
+        train_adata, labels=labels_obs, n_jobs=10, feature_selection=True, **kwargs)
+
+    # Save
+    if outfile is not None:
+        ct_model.write(outfile)
+    return(ct_model)
+
+## --- KNN classifier --- ##
+
+
+def train_weighted_knn(
+        train_adata: AnnData,
+        outfile: str = None,
+        use_rep: str = 'X_scVI',
+        n_neighbors: int = 50):
+    """Trains a weighted KNN classifier on ``train_adata.obsm[use_rep]``.
+    Parameters
+    ----------
+    train_adata: :
+        Annotated dataset to be used to train KNN classifier with ``label_key`` as the target variable.
+    outfile: path to pkl file to save trained model 
+    use_rep: 
+        Name of the obsm layer to be used for calculation of neighbors. If set to "X", anndata.X will be
+        used (default: X_scVI)
+    n_neighbors: 
+        Number of nearest neighbors in KNN classifier.
+    """
+    print(
+        f"Weighted KNN with n_neighbors = {n_neighbors} ... ",
+        end="",
+    )
+    if use_rep == "X":
+        train_emb = train_adata.X
+    elif use_rep in train_adata.obsm.keys():
+        train_emb = train_adata.obsm[use_rep]
+    else:
+        raise ValueError(
+            "use_rep should be set to either 'X' or the name of the obsm layer to be used!"
+        )
+    k_neighbors_transformer = pynndescent.NNDescent(
+        train_emb,
+        n_neighbors=n_neighbors,
+        metric="euclidean",
+        n_jobs=-1,
+    )
+    k_neighbors_transformer.prepare()
+
+    if outfile is not None:
+        with open(outfile, 'wb') as f:
+            pkl.dump(k_neighbors_transformer, f)
+    return(k_neighbors_transformer)
--- diff2atlas/uncertainty_metrics.py
+++ diff2atlas/uncertainty_metrics.py
@@ -10,114 +10,71 @@
 # from matplotlib.colors import LinearSegmentedColormap
 import numpy as np
 import pandas as pd
-# import scanpy as sc
-# import numpy.random as random
-# import scipy
-# import sklearn
-# import time
-# from scipy.spatial import cKDTree
-# import scvelo
-# from scipy.sparse import csr_matrix, issparse
-# from typing import Union, Optional, Sequence, Any, Mapping, List, Tuple
-from anndata import AnnData
-# import torch
+import scanpy as sc
 
+import scipy
+from scipy.sparse import csr_matrix, issparse
+from typing import Union, Optional, Sequence, Any, Mapping, List, Tuple
+from anndata import AnnData
+import scvi
+import torch
+import sklearn.metrics
 from collections import Counter
 from sklearn.neighbors import KNeighborsTransformer
 from pynndescent import NNDescent
 import pynndescent
+import pickle as pkl
 
 
 ## --- KNN classifier uncertainty --- ##
 # Adapting implementation in https://github.com/LungCellAtlas/mapping_data_to_the_HLCA/blob/main/scripts/scarches_label_transfer.py
 
 
-def weighted_knn_trainer(
-        train_adata: AnnData,
-        train_adata_emb: str,
-        n_neighbors: int = 50):
-    """Trains a weighted KNN classifier on ``train_adata``.
-    Parameters
-    ----------
-    train_adata: :class:`~anndata.AnnData`
-        Annotated dataset to be used to train KNN classifier with ``label_key`` as the target variable.
-    train_adata_emb: str
-        Name of the obsm layer to be used for calculation of neighbors. If set to "X", anndata.X will be
-        used
-    n_neighbors: int
-        Number of nearest neighbors in KNN classifier.
-    """
-    print(
-        f"Weighted KNN with n_neighbors = {n_neighbors} ... ",
-        end="",
-    )
-    if train_adata_emb == "X":
-        train_emb = train_adata.X
-    elif train_adata_emb in train_adata.obsm.keys():
-        train_emb = train_adata.obsm[train_adata_emb]
-    else:
-        raise ValueError(
-            "train_adata_emb should be set to either 'X' or the name of the obsm layer to be used!"
-        )
-    k_neighbors_transformer = pynndescent.NNDescent(
-        train_emb,
-        n_neighbors=n_neighbors,
-        metric="euclidean",
-        n_jobs=-1,
-    )
-    k_neighbors_transformer.prepare()
-    return k_neighbors_transformer
-
-
-def weighted_knn_transfer(
-    query_adata,
-    query_adata_emb,
-    ref_adata_obs,
-    label_keys,
-    knn_model,
-    threshold=1,
-    pred_unknown=False,
-    mode="package",
+def weighted_knn_transfer_uncertainty(
+    model: Union(NNDescent, str),
+    query_adata: AnnData,
+    train_labels,
+    use_rep: str = 'X_scVI',
+    return_labels: bool = False,
 ):
     """Annotates ``query_adata`` cells with an input trained weighted KNN classifier.
     Parameters
     ----------
+    model: :class:`~pynndescent._neighbors.NNDescent`
+        knn model trained on reference adata with models.weighted_knn_trainer function
     query_adata: :class:`~anndata.AnnData`
         Annotated dataset to be used to queryate KNN classifier. Embedding to be used
-    query_adata_emb: str
+    train_labels: list or series or array of labels from training data 
+    use_rep: str
         Name of the obsm layer to be used for label transfer. If set to "X",
         query_adata.X will be used
     ref_adata_obs: :class:`pd.DataFrame`
         obs of ref Anndata
     label_keys: str
         Names of the columns to be used as target variables (e.g. cell_type) in ``query_adata``.
-    knn_model: :class:`~sklearn.neighbors._graph.KNeighborsTransformer`
-        knn model trained on reference adata with weighted_knn_trainer function
-    threshold: float
-        Threshold of uncertainty used to annotating cells as "Unknown". cells with
-        uncertainties higher than this value will be annotated as "Unknown".
-        Set to 1 to keep all predictions. This enables one to later on play
-        with thresholds.
-    pred_unknown: bool
-        ``False`` by default. Whether to annotate any cell as "unknown" or not.
-        If `False`, ``threshold`` will not be used and each cell will be annotated
-        with the label which is the most common in its ``n_neighbors`` nearest cells.
-    mode: str
-        Has to be one of "paper" or "package". If mode is set to "package",
-        uncertainties will be 1 - P(pred_label), otherwise it will be 1 - P(true_label).
     """
-    if not type(knn_model) == NNDescent:
-        raise ValueError(
-            "knn_model should be of type pynndescent.pynndescent_.NNDescent!"
-        )
+    if type(model) == NNDescent:
+        knn_model = model
+    elif type(model) == str:
+        try:
+            with open(model, 'rb') as f:
+                knn_model = pkl.load(f)
+        except:
+            raise FileNotFoundError(
+                f'{model} should be either a trained NNDescent object or a path to a pickle file')
 
-    if query_adata_emb == "X":
+    if isinstance(train_labels, pd.Series):
+        y_train_labels = train_labels.tolist()
+    else:
+        y_train_labels = train_labels
+
+    if use_rep == "X":
         query_emb = query_adata.X
-    elif query_adata_emb in query_adata.obsm.keys():
-        query_emb = query_adata.obsm[query_adata_emb]
+    elif use_rep in query_adata.obsm.keys():
+        query_emb = query_adata.obsm[use_rep]
     else:
         raise ValueError(
-            "query_adata_emb should be set to either 'X' or the name of the obsm layer to be used!"
+            "use_rep should be set to either 'X' or the name of the obsm layer to be used!"
         )
     top_k_indices, top_k_distances = knn_model.query(
         query_emb, k=knn_model.n_neighbors)
@@ -131,281 +88,116 @@ def weighted_knn_transfer(
     weights = top_k_distances_tilda / np.sum(
         top_k_distances_tilda, axis=1, keepdims=True
     )
-    cols = ref_adata_obs.columns[ref_adata_obs.columns.str.startswith(
-        label_keys)]
-    uncertainties = pd.DataFrame(columns=cols, index=query_adata.obs_names)
-    pred_labels = pd.DataFrame(columns=cols, index=query_adata.obs_names)
+    uncertainties = pd.DataFrame(
+        columns=['pred_uncertainty'], index=query_adata.obs_names)
+    pred_labels = pd.DataFrame(
+        columns=['pred_label'], index=query_adata.obs_names)
+
     for i in range(len(weights)):
-        for j in cols:
-            y_train_labels = ref_adata_obs[j].values
-            counter = Counter(y_train_labels[top_k_indices[i]])
-            # Here I assume the highest no of neighbors also has the highest probability
-            best_label = max(counter, key=counter.get)
-            best_prob = weights[i, y_train_labels[top_k_indices[i]]
-                                == best_label].sum()
+        counter = Counter(y_train_labels[top_k_indices[i]])
+        # Here I assume the highest no of neighbors also has the highest probability
+        best_label = max(counter, key=counter.get)
+        best_prob = weights[i, y_train_labels[top_k_indices[i]]
+                            == best_label].sum()
 
-            # Annotating low probability as unknown
-            if pred_unknown:
-                if best_prob >= threshold:
-                    pred_label = best_label
-                else:
-                    pred_label = "Unknown"
-            else:
-                pred_label = best_label
-
-            uncertainties.iloc[i][j] = (max(1 - best_prob, 0))
-            pred_labels.iloc[i][j] = (pred_label)
-
-    print("finished!")
+        uncertainties.iloc[i] = (max(1 - best_prob, 0))
+        pred_labels.iloc[i] = (best_label)
 
     return pred_labels, uncertainties
 
 
-# ## --- KNN conservation --- ##
+def trueVSpred_gex_cosine(
+    model: Union(scvi.model.SCVI, str),
+    query_adata: AnnData,
+    n_samples=50,
+    seed: int = 42,
+    scale: bool = False
+):
+    '''
+    Compute cosine distance between true and predicted gene expression profile
+
+    Params:
+    ------
+    - adata: AnnData object
+    - pred_layer: element in adata.layers storing predicted gene expression profile
+    - true_layer: element in adata.layers storing true gene expression profile
+    - scale: should gex profiles be scaled before computing the distance (== calculating correlation)    
+
+    Returns:
+    -------
+    None, modifies adata in place adding `adata.obs['trueVSpred_gex_cosine']`
+    '''
+    if type(model) == scvi.model.SCVI:
+        vae = model
+    elif type(model) == str:
+        try:
+            vae = scvi.model.SCVI.load(model, adata=query_adata)
+        except:
+            raise FileNotFoundError(
+                f'{model} should be either a trained scvi.model.SCVI object or a path to a model dir')
+
+    if 'log1p' not in query_adata.uns.keys():
+        sc.pp.normalize_per_cell(query_adata)
+        sc.pp.log1p(query_adata)
+    X_true = query_adata.X.copy()
+
+    scvi.settings.seed = seed
+    post_sample = vae.posterior_predictive_sample(n_samples=n_samples)
+    post_sample = np.log1p(post_sample)
+    X_pred = post_sample.mean(2)
+
+    if scipy.sparse.issparse(X_true):
+        X_true = X_true.toarray()
+    if scipy.sparse.issparse(X_pred):
+        X_pred = X_pred.toarray()
+
+    if scale:
+        X_pred = sc.pp.scale(X_pred, zero_center=False)
+        X_true = sc.pp.scale(X_true, zero_center=False)
+
+    cosine_all = sklearn.metrics.pairwise.cosine_distances(X_true, X_pred)
+    return(np.diag(cosine_all))
 
 
-# def _knn_from_embedding(X_emb, k):
-#     if int(''.join(scipy.__version__.split("."))) >= 160:
-#         KNN_pred = cKDTree(X_emb).query(x=X_emb, k=k, workers=5)
-#     else:
-#         KNN_pred = cKDTree(X_emb).query(x=X_emb, k=k, n_jobs=5)
-#     KNN_pred_mat = np.zeros(shape=[KNN_pred[1].shape[0], KNN_pred[1].shape[0]])
-#     for i in range(KNN_pred[0].shape[0]):
-#         KNN_pred_mat[i, KNN_pred[1][i, :]] = 1
-#     return(KNN_pred_mat)
+def inference_posterior_distance(vae: scvi.model.SCVI,
+                                 adata: AnnData,
+                                 n_samples: int = 50,
+                                 seed: int = 42):
+    '''
+    Compute distance of sampled z positions to inferred (mean) z position
+    (~ uncertainty around the position in the latent space)
 
+    Params:
+    -------
+    -
 
-# def KNNconservation(adata_query, q2r_emb='X_scVI_train', q_emb='X_scVI_fit', k=50):
-#     # Find NNs in query
-#     X_emb_true = adata_query.obsm[q2r_emb].copy()
-#     KNN_true_mat = _knn_from_embedding(X_emb_true, k=k)
+    Returns:
+    --------
+    -
+    '''
 
-#     # Find NNs in common embedding
-#     X_emb_pred = adata_query.obsm[q_emb].copy()
-#     KNN_pred_mat = _knn_from_embedding(X_emb_pred, k=k)
+    scvi.settings.seed = seed
 
-#     KNN_conservation = ((KNN_pred_mat == KNN_true_mat)
-#                         & (KNN_pred_mat == 1)).sum(1)/k
-#     return(KNN_conservation)
+    # Get inferred latent dims
+    Z_dims = vae.get_latent_representation()
 
+    # Sample latent dims
+    scdl = vae._make_data_loader(adata=adata)
 
-# ## --- Model comparison metrics --- ##
+    for tensors in scdl:
+        inference_kwargs = dict(n_samples=n_samples)
+        inference_outputs, _ = vae.module.forward(
+            tensors=tensors,
+            inference_kwargs=inference_kwargs,
+            compute_loss=False,
+        )
 
-# def trueVSpred_gex_cosine(
-#     adata: AnnData,
-#     pred_layer: str,
-#     true_layer: str = 'logcounts',
-#     scale: bool = False
-# ):
-#     '''
-#     Compute cosine distance between true and predicted gene expression profile
+    # Compute distance 2 samples
+    dmat_all = torch.cdist(Z_dims, inference_outputs['z'], p=2).cpu().numpy()
+    dmat = np.diagonal(dmat_all, axis1=1, axis2=2)
 
-#     Params:
-#     ------
-#     - adata: AnnData object
-#     - pred_layer: element in adata.layers storing predicted gene expression profile
-#     - true_layer: element in adata.layers storing true gene expression profile
-#     - scale: should gex profiles be scaled before computing the distance (== calculating correlation)
-
-#     Returns:
-#     -------
-#     None, modifies adata in place adding `adata.obs['trueVSpred_gex_cosine']`
-#     '''
-#     X_true = adata.layers[true_layer].copy()
-#     if scipy.sparse.issparse(X_true):
-#         X_true = X_true.toarray()
-
-#     X_pred = adata.layers[pred_layer].copy()
-#     if scipy.sparse.issparse(X_pred):
-#         X_pred = X_pred.toarray()
-
-#     if scale:
-#         X_pred = sc.pp.scale(X_pred, zero_center=False)
-#         X_true = sc.pp.scale(X_true, zero_center=False)
-
-#     cosine_all = sklearn.metrics.pairwise.cosine_distances(X_true, X_pred)
-#     adata.obs['trueVSpred_gex_cosine'] = np.diag(cosine_all)
-
-
-# def trueVSpred_gex_euclidean(
-#     adata: AnnData,
-#     pred_layer: str,
-#     true_layer: str = 'logcounts',
-#     scale: bool = False
-# ):
-#     '''
-#     Compute cosine distance between true and predicted gene expression profile
-
-#     Params:
-#     ------
-#     - adata: AnnData object
-#     - pred_layer: element in adata.layers storing predicted gene expression profile
-#     - true_layer: element in adata.layers storing true gene expression profile
-#     - scale: should gex profiles be scaled before computing the distance
-
-#     Returns:
-#     -------
-#     None, modifies adata in place adding `adata.obs['trueVSpred_gex_cosine']`
-#     '''
-#     X_true = adata.layers[true_layer].copy()
-#     if scipy.sparse.issparse(X_true):
-#         X_true = X_true.toarray()
-
-#     X_pred = adata.layers[pred_layer].copy()
-#     if scipy.sparse.issparse(X_pred):
-#         X_pred = X_pred.toarray()
-
-#     if scale:
-#         X_pred = sc.pp.scale(X_pred, zero_center=False)
-#         X_true = sc.pp.scale(X_true, zero_center=False)
-
-#     cosine_all = sklearn.metrics.pairwise.euclidean_distances(X_true, X_pred)
-#     adata.obs['trueVSpred_gex_eucl'] = np.diag(cosine_all)
-
-
-# def trueVSpred_gex_mse(
-#     adata: AnnData,
-#     pred_layer: str,
-#     true_layer: str = 'logcounts',
-#     scale: bool = False
-# ):
-#     '''
-#     Compute cosine distance between true and predicted gene expression profile
-
-#     Params:
-#     ------
-#     - adata: AnnData object
-#     - pred_layer: element in adata.layers storing predicted gene expression profile
-#     - true_layer: element in adata.layers storing true gene expression profile
-#     - scale: should gex profiles be scaled before computing the distance
-
-#     Returns:
-#     -------
-#     None, modifies adata in place adding `adata.obs['trueVSpred_gex_cosine']`
-#     '''
-#     X_true = adata.layers[true_layer].copy()
-#     if scipy.sparse.issparse(X_true):
-#         X_true = X_true.toarray()
-
-#     X_pred = adata.layers[pred_layer].copy()
-#     if scipy.sparse.issparse(X_pred):
-#         X_pred = X_pred.toarray()
-
-#     if scale:
-#         X_pred = sc.pp.scale(X_pred, zero_center=False)
-#         X_true = sc.pp.scale(X_true, zero_center=False)
-
-#     mse = np.array([sklearn.metrics.mean_squared_error(
-#         X_true[i, :], X_pred[i, :]) for i in range(X_pred.shape[0])])
-#     adata.obs['trueVSpred_gex_mse'] = mse
-
-
-# # def trueVSpred_gex_weigthed_euclidean(
-# #     adata: AnnData,
-# #     pred_layer: str,
-# #     true_layer: str = 'logcounts'
-# # ):
-# #     '''
-# #     Compute weighted euclidean distance between true and predicted gene expression profile
-
-# #     Params:
-# #     ------
-# #     - adata: AnnData object
-# #     - pred_layer: element in adata.layers storing predicted gene expression profile
-# #     - true_layer: element in adata.layers storing true gene expression profile
-
-# #     Returns:
-# #     -------
-# #     None, modifies adata in place adding `adata.obs['trueVSpred_gex_WNN']`
-# #     '''
-# #     X_true = adata.layers[true_layer].copy()
-# #     if scipy.sparse.issparse(X_true):
-# #         X_true = X_true.toarray()
-
-# #     X_pred = adata.layers[pred_layer].copy()
-# #     if scipy.sparse.issparse(X_pred):
-# #         X_pred = X_pred.toarray()
-
-# #     # Distance as seen in Hao et al. 2020
-# #     # ...
-# #     adata.obs['trueVSpred_gex_weigthed_euclidean'] = trueVSpred_gex_weigthed_euclidean
-
-
-# def fitVStrain_error(adata_query,
-#                      pred_layer_prefix,
-#                      dist_metric='cosine'
-#                      ):
-#     '''
-#     Compute difference in reconstruction error between fit and trained models on query
-
-#     Params:
-#     ------
-#     - adata_query: anndata object of query dataset
-#     - pred_layer_prefix: prefix of type of gene expression prediction to use
-#     (i.e. from KNN imputation or posterior sampling)
-#     - dist_metric: distance metric to use to compare true and predicted gex profiles
-#     (one of 'cosine' or 'weigthed_euclidean', default: cosine)
-
-#     Returns:
-#     -------
-#     None, modifies adata_query in place adding fitVStrain_error in adata_query.obs
-#     '''
-#     if dist_metric == 'cosine':
-#         unc_function = trueVSpred_gex_cosine
-#     if dist_metric == 'weigthed_euclidean':
-#         unc_function = trueVSpred_gex_WNN
-#     else:
-#         raise ValueError(
-#             f"{dist_metric} is not a recognized distance function.\n Use one of 'cosine' or 'WNN'")
-#     true_layer = pred_layer_prefix.split("_")[1]
-#     unc_function(
-#         adata_query, pred_layer=f'{pred_layer_prefix}_fit', true_layer=true_layer)
-#     error_fit = adata_query.obs[f'trueVSpred_gex_{dist_metric}'].copy()
-#     unc_function(
-#         adata_query, pred_layer=f'{pred_layer_prefix}_train', true_layer=true_layer)
-#     error_train = adata_query.obs[f'trueVSpred_gex_{dist_metric}'].copy()
-#     adata_query.obs[f'fitVStrain_error_{pred_layer_prefix}_{dist_metric}'] = error_fit - error_train
-
-
-# def inference_posterior_distance(vae: scvi.model.SCVI,
-#                                  adata: AnnData,
-#                                  n_samples: int = 50,
-#                                  seed: int = 42):
-#     '''
-#     Compute distance of sampled z positions to inferred (mean) z position
-#     (~ uncertainty around the position in the latent space)
-
-#     Params:
-#     -------
-#     -
-
-#     Returns:
-#     --------
-#     -
-#     '''
-
-#     scvi.settings.seed = seed
-
-#     # Get inferred latent dims
-#     Z_dims = vae.get_latent_representation()
-
-#     # Sample latent dims
-#     scdl = vae._make_data_loader(adata=adata)
-
-#     for tensors in scdl:
-#         inference_kwargs = dict(n_samples=n_samples)
-#         inference_outputs, _ = vae.module.forward(
-#             tensors=tensors,
-#             inference_kwargs=inference_kwargs,
-#             compute_loss=False,
-#         )
-
-#     # Compute distance 2 samples
-#     dmat_all = torch.cdist(Z_dims, inference_outputs['z'], p=2).cpu().numpy()
-#     dmat = np.diagonal(dmat_all, axis1=1, axis2=2)
-
-#     adata.obsm['dist_z_samples'] = dmat.T
-#     adata.obs['mean_dist_z_samples'] = dmat.mean(0)
+    adata.obsm['dist_z_samples'] = dmat.T
+    adata.obs['mean_dist_z_samples'] = dmat.mean(0)
 
 
 ## --- old code --- ##
