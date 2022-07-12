34a89b26d093378fc5349b5c5936711500b0ac34
Emma Dann <ed6@sanger.ac.uk>
2022-07-12 16:55:12 +0100

    save updated design comparison

--- scripts/submit_design_comparison.sh
+++ scripts/submit_design_comparison.sh
@@ -5,17 +5,18 @@ cd /nfs/team205/ed6/bin/diff2atlas/scripts
 
 outdir=/lustre/scratch117/cellgen/team205/ed6/PBMC_CZI_integration_filtered/
 
-for p in 'naive_B_cell' 'platelet' 'CD56_bright_natural_killer_cell_human' 'mature_NK_T_cell' 'CD14_positive_monocyte'; do
+for p in $(cat /lustre/scratch117/cellgen/team205/ed6/PBMC_CZI_integration_filtered/PBMC_merged.normal.subsample500cells.clean_celltypes.txt); do
     echo "python run_design_comparison.py PBMC_merged.normal.subsample500cells.clean_celltypes.h5ad ${p} --outpath ${outdir}" | \
         bsub -G teichlab -o logfile-scvi-%J.out -e logfile-scvi-%J.err -q gpu-normal -M25000 -R "select[mem>25000] rusage[mem=25000]" -gpu "mode=shared:j_exclusive=no:gmem=6000:num=1" 
 done
 
-for d in $(ls -d $outdir/*/); do
-    echo "python run_design_merge.py ${outdir}${d}/" | \
+for d in $(ls -d $outdir*/); do
+    echo "python run_design_merge.py ${d}/" | \
         bsub -G teichlab -o logfile-merge-%J.out -e logfile-merge-%J.err -q gpu-normal -M50000 -R "select[mem>50000] rusage[mem=50000]" -gpu "mode=shared:j_exclusive=no:gmem=6000:num=1" 
 done
 
-for d in $(ls -d $outdir/*/); do
-    echo "python run_milo.py ${outdir}${d}/" | \
+for d in $(ls -d $outdir*/); do
+    echo "python run_milo.py ${d}/" | \
         bsub -G teichlab -o logfile-milo-%J.out -e logfile-milo-%J.err -q gpu-normal -M30000 -R "select[mem>30000] rusage[mem=30000]" -gpu "mode=shared:j_exclusive=no:gmem=6000:num=1" 
 done
+
