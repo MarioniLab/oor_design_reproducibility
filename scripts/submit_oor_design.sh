88b733a8c4b44358fe4cd5e93960c4d31f2c05a5
Emma Dann <ed6@sanger.ac.uk>
2022-09-28 10:20:42 +0100

    updated ctrl size analysis

--- scripts/submit_oor_design.sh
+++ scripts/submit_oor_design.sh
@@ -11,3 +11,9 @@ for p in $(cat /lustre/scratch117/cellgen/team205/ed6/PBMC_CZI_integration_filte
             bsub -G teichlab -o logfile-scvi-%J.out -e logfile-scvi-%J.err -q gpu-normal -M50000 -R "select[mem>50000] rusage[mem=50000]" -gpu "mode=shared:j_exclusive=no:gmem=6000:num=1" 
     done
 done
+
+## Test AR design requirements
+p=central_memory_CD4_positive_alpha_beta_T_cell
+d=AR
+echo "python run_oor_design.py PBMC_merged.normal.subsample500cells.clean_celltypes.h5ad ${p} ${d} --outpath ${outdir}" | \
+            bsub -G teichlab -o logfile-scvi-%J.out -e logfile-scvi-%J.err -q gpu-normal -M200000 -R "select[mem>200000] rusage[mem=200000]" -gpu "mode=shared:j_exclusive=no:gmem=6000:num=1" 
