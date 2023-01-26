a74c0252b7db170dd0f1ea840759fb8032f4a67a
Emma Dann <ed6@sanger.ac.uk>
2023-01-26 14:26:35 +0000

    start imperfect atlas analysis

---
+++ src/_misc/imperfect_atlas/train_atlas.py
@@ -0,0 +1,26 @@
+import argparse
+from multiprocessing.sharedctypes import Value
+import scanpy as sc
+
+from oor_benchmark.methods._latent_embedding import embedding_scvi
+
+parser = argparse.ArgumentParser()
+parser.add_argument("h5ad_file",
+                    type=str,
+                    help="path to input anndata file")
+parser.add_argument("--outdir",
+                    default='/lustre/scratch117/cellgen/team205/ed6/OOR_benchmark_fetal/',
+                    help="Output directory")
+parser.add_argument("--testing", action='store_true',
+                    help="Run in testing mode")
+args = parser.parse_args()
+
+# unpack arguments
+h5ad_file = args.h5ad_file
+outdir = args.outdir
+
+adata = sc.read_h5ad(outdir + h5ad_file)
+adata.obs['dataset_group'] = 'atlas'
+
+embedding_scvi(adata, n_hvgs = 5000, outdir=outdir, batch_key="sample_id", train_params={'max_epochs':400})
+adata.write_h5ad(outdir + h5ad_file.replace('.h5ad', '_scvi.h5ad'))
